0313c57bbe07c617205135ad6b43563c85c937c5
rfechtner <ronfechtner@gmail.com>
2020-02-08 04:45:58 +0100

    Updated tests for new webflow instance

--- tests/test_Webflow.py
+++ tests/test_Webflow.py
@@ -1,9 +1,12 @@
+import os
 from webflowpy.Webflow import Webflow
 
 def test_webflow_config():
     webflow = Webflow()
 
-    assert webflow.token == 'd9d9faf535a13927a412f968f6e11e465beb44511824c4e48090ef68b3ac36fa'
+    # Set by environment variable 'WEBFLOW_API_KEY'
+    assert webflow.token != ''
+
     assert webflow.version == '1.0.0'
 
 def test_webflow_info():
@@ -16,27 +19,27 @@ def test_webflow_info():
 def test_webflow_sites():
     webflow = Webflow()
 
-    resp = webflow.site('5c2601f89a1575861286a249')
+    resp = webflow.site(os.getenv('SITE_ID'))
 
     assert resp['shortName'] == 'webflowpy-tester'
 
 def test_webflow_collection():
     webflow = Webflow()
 
-    resp = webflow.collection('5c26497f4fdbbae398c05031')
+    resp = webflow.collection(os.getenv('COLLECTION_ID'))
 
     assert resp['slug'] == 'tests'
 
 def test_webflow_items_limit_10_offset_2():
     webflow = Webflow()
 
-    resp = webflow.items('5c26497f4fdbbae398c05031', limit=10, offset=2)
+    resp = webflow.items(os.getenv('COLLECTION_ID'), limit=10, offset=2)
 
-    assert resp['items'][0]['name'] == 'Sint Error'
+    assert resp['items'][0]['name'] == 'Fugiat Amet'
 
 def test_webflow_items_all():
     webflow = Webflow()
 
-    resp = webflow.items('5c26497f4fdbbae398c05031', all = True)
+    resp = webflow.items(os.getenv('COLLECTION_ID'), all = True)
 
-    assert int(resp['count']) == 22
+    assert int(resp['count']) == 20
