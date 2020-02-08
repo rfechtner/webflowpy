d9570aaf81ec7a207387379f4a1b3119c6c29c24
rfechtner <ronfechtner@gmail.com>
2020-02-08 05:15:36 +0100

    Updated tests for new webflow instance

--- tests/test_Webflow.py
+++ tests/test_Webflow.py
@@ -43,3 +43,42 @@ def test_webflow_items_all():
     resp = webflow.items(os.getenv('COLLECTION_ID'), all = True)
 
     assert int(resp['count']) == 20
+
+def test_webflow_items_get_create_update_remove():
+    webflow = Webflow()
+
+    # Fetch an item
+    resp = webflow.item(os.getenv('COLLECTION_ID'), os.getenv('ITEM_ID'))
+    item_name = resp['items'][0]['name']
+
+    assert resp['count'] == 1
+    assert item_name == "Id"
+
+    # Create new live item
+    new_name = item_name + '-test'
+    new_item = {
+        'name': new_name,
+        'slug': 'test_create',
+        '_archived': False,
+        '_draft': False
+    }
+    resp = webflow.createItem(os.getenv('COLLECTION_ID'), new_item, live=True)
+    new_item_id = resp['_id']
+    assert resp['name'] == new_name
+
+    # Update live item
+    update_item = {
+        'name': new_name + "-update",
+        'slug': 'test_create',
+        '_archived': False,
+        '_draft': False
+    }
+
+    resp = webflow.updateItem(os.getenv('COLLECTION_ID'), new_item_id, update_item, live = True)
+
+    assert int(resp['name']) == new_name + "-update"
+
+    # Delete item
+    resp = webflow.removeItem(os.getenv('COLLECTION_ID'), new_item_id)
+
+    assert resp['deleted'] == 1
